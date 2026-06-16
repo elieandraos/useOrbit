b21ffcde9980fe0f99ea56f7c1e3e824457f5118
Elie Andraos <elieandraos31@gmail.com>
2026-06-16 10:40:39 +0300

    rewrite UserMenuContent without reka-ui dropdown

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import { Link, router } from '@inertiajs/vue3';
 import { LogOut, Settings } from '@lucide/vue';
-import {
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
 import UserInfo from '@/components/UserInfo.vue';
 import { logout } from '@/routes';
 import { edit } from '@/routes/profile';
@@ -24,31 +19,27 @@ defineProps<Props>();
 </script>
 
 <template>
-    <DropdownMenuLabel class="p-0 font-normal">
-        <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-            <UserInfo :user="user" :show-email="true" />
-        </div>
-    </DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuGroup>
-        <DropdownMenuItem :as-child="true">
-            <Link class="block w-full cursor-pointer" :href="edit()" prefetch>
-                <Settings class="mr-2 h-4 w-4" />
-                Settings
-            </Link>
-        </DropdownMenuItem>
-    </DropdownMenuGroup>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem :as-child="true">
-        <Link
-            class="block w-full cursor-pointer"
-            :href="logout()"
-            @click="handleLogout"
-            as="button"
-            data-test="logout-button"
-        >
-            <LogOut class="mr-2 h-4 w-4" />
-            Log out
-        </Link>
-    </DropdownMenuItem>
+    <div class="flex items-center gap-2 px-2 py-1.5 text-left text-sm">
+        <UserInfo :user="user" :show-email="true" />
+    </div>
+    <Separator class="my-1" />
+    <Link
+        class="flex items-center rounded-[6px] px-2 py-1.5 text-sm text-primary hover:bg-sunken"
+        :href="edit()"
+        prefetch
+    >
+        <Settings class="mr-2 h-4 w-4" />
+        Settings
+    </Link>
+    <Separator class="my-1" />
+    <Link
+        class="flex w-full items-center rounded-[6px] px-2 py-1.5 text-left text-sm text-secondary hover:bg-sunken"
+        :href="logout()"
+        @click="handleLogout"
+        as="button"
+        data-test="logout-button"
+    >
+        <LogOut class="mr-2 h-4 w-4" />
+        Log out
+    </Link>
 </template>
