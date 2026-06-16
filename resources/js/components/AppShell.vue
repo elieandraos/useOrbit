c088e894593505c1f985d768814eed2d2406effa
Elie Andraos <elieandraos31@gmail.com>
2026-06-16 10:30:02 +0300

    simplify AppShell and AppContent, remove AppVariant type

--- resources/js/components/AppShell.vue
+++ resources/js/components/AppShell.vue
@@ -1,24 +1,5 @@
-<script setup lang="ts">
-import { usePage } from '@inertiajs/vue3';
-import { SidebarProvider } from '@/components/ui/sidebar';
-import type { AppVariant } from '@/types';
-
-type Props = {
-    variant?: AppVariant;
-};
-
-withDefaults(defineProps<Props>(), {
-    variant: 'sidebar',
-});
-
-const isOpen = usePage().props.sidebarOpen;
-</script>
-
 <template>
-    <div v-if="variant === 'header'" class="flex min-h-screen w-full flex-col">
+    <div class="flex min-h-screen flex-col bg-app">
         <slot />
     </div>
-    <SidebarProvider v-else :default-open="isOpen">
-        <slot />
-    </SidebarProvider>
 </template>
