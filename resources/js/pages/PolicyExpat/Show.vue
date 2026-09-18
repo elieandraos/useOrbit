ae99222aa98057112052e6c90a7c9bdde201e6ec
Elie Andraos <elieandraos31@gmail.com>
2026-09-18 09:45:00 +0300

    Build the Expat policy show page
    Refs #352

--- resources/js/pages/PolicyExpat/Show.vue
+++ resources/js/pages/PolicyExpat/Show.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
+import CoverageCard from './partials/CoverageCard.vue';
+import CoveragePeriodCard from './partials/CoveragePeriodCard.vue';
+import ExpatDetailCard from './partials/ExpatDetailCard.vue';
+import FinancialsCard from './partials/FinancialsCard.vue';
+import PartiesCard from './partials/PartiesCard.vue';
+import type { PolicyExpatResource } from './partials/policy';
+import PolicyExpatDetailShell from './partials/PolicyExpatDetailShell.vue';
+import StatusOriginCard from './partials/StatusOriginCard.vue';
 
 defineProps<{
-    policy: Record<string, unknown>;
+    policy: PolicyExpatResource;
 }>();
 </script>
 
 <template>
-    <Head title="Policy" />
-
-    <div class="flex flex-1 flex-col" />
+    <PolicyExpatDetailShell :policy="policy">
+        <div class="grid grid-cols-1 items-start gap-5 pt-6 lg:grid-cols-2">
+            <CoverageCard :policy="policy" />
+            <PartiesCard :policy="policy" />
+            <CoveragePeriodCard :policy="policy" />
+            <FinancialsCard :policy="policy" />
+            <StatusOriginCard :policy="policy" />
+            <ExpatDetailCard :policy="policy" />
+        </div>
+    </PolicyExpatDetailShell>
 </template>
